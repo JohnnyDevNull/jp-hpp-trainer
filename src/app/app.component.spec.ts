c071988a4ca59e2ced3b5b0fd182a78d27396561
JohnnyDevNull <philipp.john@live.de>
2022-09-04 22:28:28 +0200

    test: app comp should invoke loading process

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,22 +1,36 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { DataFacadeService } from './data-facade.service';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let service: DataFacadeService;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [AppComponent],
-      providers: [{ provide: DataFacadeService, useValue: {} }],
+      providers: [
+        {
+          provide: DataFacadeService,
+          useValue: {
+            load: jest.fn(),
+          },
+        },
+      ],
       schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    service = TestBed.inject(DataFacadeService);
+    fixture.detectChanges();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('should create the app and invoke the loading', () => {
+    expect(component).toBeTruthy();
+    expect(service.load).toHaveBeenCalled();
   });
 });
