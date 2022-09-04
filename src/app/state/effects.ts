66cc2d660ea2415287f12bb8f0eaef40bd778ffe
JohnnyDevNull <philipp.john@live.de>
2022-09-04 22:46:43 +0200

    feat: log error action result to console

--- src/app/state/effects.ts
+++ src/app/state/effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, exhaustMap, map, of } from 'rxjs';
+import { catchError, exhaustMap, map, of, tap } from 'rxjs';
 import { DataService } from '../data.service';
 import { LoadData, LoadDataFailure, LoadDataSuccess } from './actions';
 
@@ -16,6 +16,15 @@ export class AppEffects {
         ),
       ),
     ),
+  );
+
+  loadDataFailure$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(LoadDataFailure),
+        tap(action => console.error(action.payload)),
+      ),
+    { dispatch: false },
   );
 
   constructor(private readonly actions$: Actions, private readonly dataService: DataService) {}
